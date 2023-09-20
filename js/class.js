dce6b94714c75a56c22c5bf0acd93cafec054af9
aradmirza <aradmirza3@gmail.com>
2023-09-20 22:51:01 +0600

    javascript add

---
+++ js/class.js
@@ -0,0 +1,8 @@
+
+document.getElementById('cls-1').addEventListener('click', function(){
+  document.getElementById('tex-1').style.display = "none";
+  location.href = "https://drive.google.com/file/d/1pWgdlS0bDD-ZAmTlr5DWgK2tU2OlB-da";
+  
+
+
+})
