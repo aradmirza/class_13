4a7fc59cd2ae56f05a51387e838d224446eb4a64
aradmirza <aradmirza3@gmail.com>
2023-09-21 00:49:48 +0600

    Class 6 to 13 All Class Add In Javascript And Talwind Cass Design

--- js/class.js
+++ js/class.js
@@ -45,3 +45,60 @@ document.getElementById('cls-5').addEventListener('click', function(){
   })
 
 
+  // Class -6th
+
+document.getElementById('cls-6').addEventListener('click', function(){
+  document.getElementById('tex-6').style.display = "none";
+  location.href = "https://drive.google.com/file/d/1Pq6mvSQsfE04sbFnv-zNvy8u98hOEOqH";
+  })
+
+
+  // Class - 7th
+
+document.getElementById('cls-7').addEventListener('click', function(){
+  document.getElementById('tex-7').style.display = "none";
+  location.href = "https://drive.google.com/file/d/1AuZSi0ZqSWf7ijC68NZI2yuo405DpJkc";
+  })
+
+  // Class - 8th
+
+document.getElementById('cls-8').addEventListener('click', function(){
+  document.getElementById('tex-8').style.display = "none";
+  location.href = "https://drive.google.com/file/d/1AuZSi0ZqSWf7ijC68NZI2yuo405DpJkc";
+  })
+
+  // Class - 9
+
+document.getElementById('cls-9').addEventListener('click', function(){
+  document.getElementById('tex-9').style.display = "none";
+  location.href = "https://drive.google.com/file/d/1wm0L4qQdgvrGtoi8H33H2SL5Y0ct6lzr";
+  })
+
+  // Class - 10
+
+document.getElementById('cls-10').addEventListener('click', function(){
+  document.getElementById('tex-10').style.display = "none";
+  location.href = "https://drive.google.com/file/d/1XVlBZIS4xQ8Gza3Abbuy6Oq7Nj6RRlBj";
+  })
+
+  // Class - 11
+
+document.getElementById('cls-11').addEventListener('click', function(){
+  document.getElementById('tex-11').style.display = "none";
+  location.href = "https://drive.google.com/file/d/1Z2saVwLgcx2pLSxl6g-HEF7i3cqpy4f0";
+  })
+
+  // Class - 12
+
+document.getElementById('cls-12').addEventListener('click', function(){
+  document.getElementById('tex-12').style.display = "none";
+  location.href = "https://drive.google.com/file/d/16F2dS99omuchZ9FzBQ6OucKqydiikgvK";
+  })
+
+  // Class - 13
+
+document.getElementById('cls-13').addEventListener('click', function(){
+  document.getElementById('tex-13').style.display = "none";
+  location.href = "https://drive.google.com/file/d/1CLvpEGAJMrZ8pc0w8OO8uSwRQEaLZBfW";
+  })
+
