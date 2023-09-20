a9d62f4ad410a269d5ddff53eef4be5d46aec1cf
aradmirza <aradmirza3@gmail.com>
2023-09-21 00:25:25 +0600

    Class 1 to 5 Add And Some Javascript function add working

--- js/class.js
+++ js/class.js
@@ -1,8 +1,47 @@
 
+
+// Class - 1st
+
 document.getElementById('cls-1').addEventListener('click', function(){
   document.getElementById('tex-1').style.display = "none";
-  location.href = "https://drive.google.com/file/d/1pWgdlS0bDD-ZAmTlr5DWgK2tU2OlB-da";
-  
+  location.href = "https://drive.google.com/file/d/1_Neq6BYUDskV2Wm5i1LyAciB7Rx004aw";
+  })
+
+// Class - 2nd
+
+document.getElementById('cls-2').addEventListener('click', function(){
+  document.getElementById('tex-2').style.display = "none";
+  location.href = "https://drive.google.com/file/d/1_Neq6BYUDskV2Wm5i1LyAciB7Rx004aw";
+  })
 
 
-})
+  // Class -3rd
+
+document.getElementById('cls-3').addEventListener('click', function(){
+  document.getElementById('tex-3').style.display = "none";
+  location.href = "https://drive.google.com/file/d/1Jvn-QC-tFSCIhBvGOJDDhokCarr9smZc";
+  })
+
+
+  // Class - 4th
+
+document.getElementById('cls-4').addEventListener('click', function(){
+  document.getElementById('tex-4').style.display = "none";
+  location.href = "https://drive.google.com/file/d/1FItujoRpPW3aQtkS2a2hR9SAywPmT9LR";
+  })
+
+  // Class - 4th
+
+document.getElementById('cls-4').addEventListener('click', function(){
+  document.getElementById('tex-4').style.display = "none";
+  location.href = "https://drive.google.com/file/d/1FItujoRpPW3aQtkS2a2hR9SAywPmT9LR";
+  })
+
+// Class - 5th
+
+document.getElementById('cls-5').addEventListener('click', function(){
+  document.getElementById('tex-5').style.display = "none";
+  location.href = "https://drive.google.com/file/d/1cAzrlTIrhsuid-MIDL5vC4HAqhsR3vrv";
+  })
+
+
